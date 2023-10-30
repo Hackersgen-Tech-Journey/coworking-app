b13af21c2ebfdf94215d81246faffcaa6cae5aa3
Ivan Pesenti <ipesenti@sorint.com>
2023-10-30 12:07:40 +0100

    fixed routing configs client/server

--- app/src/composables/useAxios/index.js
+++ app/src/composables/useAxios/index.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 export function useAxios() {
-  const instance = axios.create({
-    baseURL: "http://localhost:3030",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  });
-  instance.interceptors.request.use((config) => {
-    const token = localStorage.getItem("coworking-token");
-    if (token) {
-      config.headers["X-Auth-Token"] = token;
-    }
-    return config;
-  });
+    const instance = axios.create({
+        baseURL: "http://localhost:8080",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+    instance.interceptors.request.use((config) => {
+        const token = localStorage.getItem("coworking-token");
+        if (token) {
+            config.headers["X-Auth-Token"] = token;
+        }
+        return config;
+    });
 
-  const sendRequest = (config) => instance.request(config);
+    const sendRequest = (config) => instance.request(config);
 
-  return { sendRequest };
+    return { sendRequest };
 }
